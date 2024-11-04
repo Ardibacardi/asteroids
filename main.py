e06c985151ed474d45d59cfec37cbf5b0f454ea3
Ardibacardi <aaamoonen@hotmail.com>
2024-11-04 13:16:43 +0100

    grouped the updatable and drawable objects

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2, )
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        player.update(dt)
-        
+
+        for obj in updatable:    
+            obj.update(dt)
+
         screen.fill(000000)
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # set fps limit
